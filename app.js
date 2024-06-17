01e39cb94b2d3d81f200d168c3db07044e04ff4f
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-06-18 00:52:00 +0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+let numberHours = document.getElementById('#number-hours');
+let barSeconds = document.getElementById('#bar-seconds');
+
+let handHours = document.getElementById('#hand1');
+let handMinutes = document.getElementById('.hand2');
+let handSeconds = document.getElementById('.hand3');
+let numberElement = [];
+let barElement = [];
+
+
+
+
+
+
+
+
